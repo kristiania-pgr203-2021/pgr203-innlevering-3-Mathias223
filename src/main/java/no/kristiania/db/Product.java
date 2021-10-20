ed6fed6b3dddf049f79a4ebfba67c1d369d2c887
Ellaoui94 <74535066+Ellaoui94@users.noreply.github.com>
2021-10-20 16:11:45 +0200

    Fields added in Product

--- src/main/java/no/kristiania/db/Product.java
+++ src/main/java/no/kristiania/db/Product.java
@@ -1,4 +1,43 @@
 package no.kristiania.db;
 
 public class Product {
+    private String productName;
+    private String productInfo;
+    private int Price;
+    private Long id;
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductInfo() {
+        return productInfo;
+    }
+
+    public void setProductInfo(String productInfo) {
+        this.productInfo = productInfo;
+    }
+
+    public int getPrice() {
+        return Price;
+    }
+
+    public void setPrice(int price) {
+        Price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+
 }
