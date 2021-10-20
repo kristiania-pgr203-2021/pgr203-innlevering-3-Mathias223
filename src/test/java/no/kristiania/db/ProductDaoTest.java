b09d0c9e82a12d626f3be7c55b22970514dfe8d4
Mathias223 <mathiastandstad@gmail.com>
2021-10-20 16:54:48 +0200

    shouldRetrieveSavedProduct(Fail)

--- src/test/java/no/kristiania/db/ProductDaoTest.java
+++ src/test/java/no/kristiania/db/ProductDaoTest.java
@@ -1,4 +1,23 @@
 package no.kristiania.db;
 
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
 public class ProductDaoTest {
+
+    @Test
+    void shouldRetrieveSavedProduct() throws SQLException {
+        ProductDao dao = new ProductDao(createDataSource());
+
+        Product product = randomProduct();
+        dao.save(product);
+
+        assertThat(dao.retrieve(product.getId()))
+                .hasNoNullFieldsOrProperties()
+                .usingRecursiveComparison()
+                .isEqualTo(product);
+
+    }
+
 }
