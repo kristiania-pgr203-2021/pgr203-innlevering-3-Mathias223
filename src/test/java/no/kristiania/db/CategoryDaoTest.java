a699d743e281abbd67f9ebc0e8802a1efad613b9
Ellaoui94 <74535066+Ellaoui94@users.noreply.github.com>
2021-10-23 13:46:41 +0200

    CategoryDaoTest updatet

--- src/test/java/no/kristiania/db/CategoryDaoTest.java
+++ src/test/java/no/kristiania/db/CategoryDaoTest.java
@@ -15,11 +15,6 @@ public class CategoryDaoTest {
     public CategoryDaoTest() throws IOException {
     }
 
-    @Test
-    void shouldListProductAndCategory(){
-        /*might no be needed*/
-    }
-
 
    @Test
     void shouldListSavedCategory() throws SQLException {
